bcd465ee03800fea561dba64009bef495073f84e
ckshei <cks856@gmail.com>
2016-10-23 13:58:07 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path') 
-
+ 
 describe('index', () => {
   jsdom({
     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
